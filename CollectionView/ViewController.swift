ddfaf0f4d53b36cc3c451fedb0707d0ac7f5241a
Derek Fitzer <franzderek@gmail.com>
2023-10-19 12:11:45 -0400

    Initial Commit

---
+++ CollectionView/ViewController.swift
@@ -0,0 +1,93 @@
+//
+//  ViewController.swift
+//  MasterMindThree
+//
+//  Created by Derek Fitzer on 10/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    let columnLayout = ColumnFlowLayout(
+        cellsPerRow: 6,
+        minimumInteritemSpacing: 10,
+        minimumLineSpacing: 10,
+        sectionInset: UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+    )
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    
+    
+    override func viewDidLoad(){
+        super.viewDidLoad()
+        collectionView?.collectionViewLayout = columnLayout
+        collectionView?.contentInsetAdjustmentBehavior = .always
+        collectionView?.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "Cell")
+        view.backgroundColor = .black
+        
+    }
+    
+    
+    // MARK: - Collection View
+    
+    
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+       // return cellImages.count
+        return 10 // used to test
+    }
+    
+    
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath) as! MyCollectionViewCell
+        //        cell.cellImage.image = UIImage(named: cellImages[indexPath.row])
+        cell.cellImage.image = UIImage(named: "gemBlackout") // call 1 imago over and over to test.
+        return cell
+    }
+}
+
+// MARK: - Column Flow
+class ColumnFlowLayout: UICollectionViewFlowLayout {
+    
+    let cellsPerRow: Int
+    
+    init(cellsPerRow: Int, minimumInteritemSpacing: CGFloat = 0, minimumLineSpacing: CGFloat = 0, sectionInset: UIEdgeInsets = .zero) {
+        self.cellsPerRow = cellsPerRow
+        super.init()
+        
+        self.minimumInteritemSpacing = minimumInteritemSpacing
+        self.minimumLineSpacing = minimumLineSpacing
+        self.sectionInset = sectionInset
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func prepare() {
+        super.prepare()
+        
+        guard let collectionView = collectionView else { return }
+        let marginsAndInsets = sectionInset.left + sectionInset.right + collectionView.safeAreaInsets.left + collectionView.safeAreaInsets.right + minimumInteritemSpacing * CGFloat(cellsPerRow - 1)
+        let itemWidth = ((collectionView.bounds.size.width - marginsAndInsets) / CGFloat(cellsPerRow)).rounded(.down)
+        itemSize = CGSize(width: itemWidth, height: itemWidth)
+    }
+    
+    override func invalidationContext(forBoundsChange newBounds: CGRect) -> UICollectionViewLayoutInvalidationContext {
+        let context = super.invalidationContext(forBoundsChange: newBounds) as! UICollectionViewFlowLayoutInvalidationContext
+        context.invalidateFlowLayoutDelegateMetrics = newBounds.size != collectionView?.bounds.size
+        return context
+    }
+    
+    
+    
+}
+
+
